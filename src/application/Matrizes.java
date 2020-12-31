317f1be3ee7ccd96a557774118449acaed5c84f7
Pablo Ryan <pabloryanbr0@gmail.com>
2020-12-31 10:29:30 -0300

    Exercicio resolvido

---
+++ src/application/Matrizes.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Scanner;
+
+public class Matrizes {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int m = sc.nextInt(); // linha
+		int n = sc.nextInt(); // coluna
+		
+		int matriz[][] = new int[m][n];
+		
+		for(int i=0;i<m;i++) {
+			for (int j=0;j<n;j++) {
+				matriz[i][j] = sc.nextInt(); 
+			}
+		}
+
+		int x = sc.nextInt(); // Número que vai ser procurado na matriz
+		
+		for(int i=0;i<m;i++) {
+			for (int j=0;j<n;j++) {
+				if(x == matriz[i][j]) {
+					System.out.println("Position " + i + ", " + j + ": ");
+					if(j != 0) {
+						System.out.println("Left: " + matriz[i][j-1]);
+					}
+					if(j != (n-1)) {
+						System.out.println("Right: " + matriz[i][j+1]);
+					}
+					if(i != 0) {
+						System.out.println("Up: " + matriz[i-1][j]);
+					}
+					if(i != (m-1)) {
+						System.out.println("Down: " + matriz[i+1][j]);
+					}
+				}
+			}
+		}
+		sc.close();
+	}
+}
+
